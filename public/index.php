53dfcd4af7f3c0046cb5644e8d1fd5f9d7710d24
ChristosK. <christoskanotidis@gmail.com>
2025-08-05 15:43:42 +0300

    public index redirection to setup page if lock file is missing

--- public/index.php
+++ public/index.php
@@ -3,18 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+$lockFilePath = __DIR__ . '/../storage/installed.lock';
+if (! file_exists($lockFilePath) && strpos($_SERVER['REQUEST_URI'], '/setup') === false) {
+    header('Location: /setup');
+    exit;
+}
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
