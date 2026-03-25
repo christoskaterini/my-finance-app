8844a6e274d3d53e6094d785da42ae0e474b88e9
ChristosK. <99069049+christoskaterini@users.noreply.github.com>
2026-03-25 12:46:28 +0200

    add trustProxies for cloudflare

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\CheckAdminRole::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
